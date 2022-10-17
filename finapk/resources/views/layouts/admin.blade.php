02642ae64c6a7fc2686c397457f6ff555d70a8d9
Heltzel <heltzel.service@hotmail.com>
2022-10-17 15:54:25 +0200

    Create Admin Layout

--- finapk/resources/views/layouts/admin.blade.php
+++ finapk/resources/views/layouts/admin.blade.php
@@ -5,9 +5,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="shortcut icon" href="{{ URL('img/finapk.png') }}" type="image/x-icon">
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -16,9 +15,17 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
-<body>
-    <div class="font-sans text-gray-900 antialiased">
-        {{ $slot }}
+<body class="font-sans antialiased">
+    <div class="min-h-screen bg-gray-100">
+        @include('layouts.navigation')
+
+        <!-- Page Heading -->
+
+
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
     </div>
 </body>
 
